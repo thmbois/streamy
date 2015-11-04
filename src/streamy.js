3d02dd2fc43ba1dae4b2e27363282e96d8fb13af
Jonas Faber <jonas.faber@iem.thm.de>
2015-11-04 18:52:59 +0100

    Add Toggle for Cam

--- src/streamy.js
+++ src/streamy.js
@@ -14,7 +14,7 @@ if(Meteor.isClient){
     // Callback: Complete, called when the countdown has reached 0
     completed: function() {
       //console.log(new Date().toString()+": Reset");
-      var currentBox = Session.get("currentBox")
+      var currentBox = Session.get("currentBox");
       var count;
       if(currentBox<InfoBoxes.find().count()-1){
         count = currentBox+1;
@@ -29,6 +29,9 @@ if(Meteor.isClient){
   Template.camarea.helpers({
     InfoBoxes: function(){
       return InfoBoxes.find();
+    },
+    CamActive: function(){
+      return Controller.findOne().config.camActive;
     }
   });
 
