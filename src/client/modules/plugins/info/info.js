d24b56b67efff81d6dd9f54fa1f5c6232c6f426d
Jonas Faber <jonas.faber@iem.thm.de>
2016-02-15 01:30:55 +0100

    Try to fix animation.

--- src/client/modules/plugins/info/info.js
+++ src/client/modules/plugins/info/info.js
@@ -1,31 +1,36 @@
-Session.setDefault("activeInfo", 0);
 
-var countdownInfo = new ReactiveCountdown(20, {
-  // Callback: Complete, called when the countdown has reached 0
-  completed: function() {
-    //console.log(new Date().toString()+": Reset");
-    var activeInfo = Session.get("activeInfo");
-    var count;
-    if(activeInfo<Info.find().count()-1){
-      count = activeInfo+1;
-    }else{
-      count = 0;
+Template.info.created = function(){
+  // counter starts at 0
+  this.state = new ReactiveDict();
+  this.state.set('activeInfo', 0);
+  var state = this.state;
+  var countdownInfo = new ReactiveCountdown(10, {
+    // Callback: Complete, called when the countdown has reached 0
+    completed: function() {
+      var counter = state.get('activeInfo');
+      if(counter<Info.find({plugin_id:"Ydh2WPDmvPiRqc4MZ"}).count()-1){
+        counter ++;
+      }else{
+        counter = 0;
+      }
+      state.set('activeInfo',counter);
+      countdownInfo.start();
     }
-    Session.set("activeInfo", count);
-    countdownInfo.start();
-  }
-});
-
-countdownInfo.start();
+  });
+  countdownInfo.start();
+};
 
 Template.info.helpers({
   info: function(){
-    return Info.find();
+    return Info.find({plugin_id:"Ydh2WPDmvPiRqc4MZ"});
   }
 });
 
 Template.infoItem.helpers({
+
   active: function(){
-    return Session.equals("activeInfo", this.infoNumber);
+    //console.log(this.text+": "+(Template.instance().parent().state.get('activeInfo')==this.infoNumber));
+    console.log(Template.instance().parent().state.get('activeInfo'));
+    return Template.instance().parent().state.equals('activeInfo',this.infoNumber) ;
   }
 });
