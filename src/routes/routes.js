b39528be769574d480f02ec4419e5b0955d2227a
Rene Diwisch <rene.diwisch@gmail.com>
2016-02-29 01:26:37 +0100

    Template Meteor ready. Modules for each Template detailed seperated. All Admin Templates created. All Client Templates created. Routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,3 +12,23 @@ Router.route('/overlay-raw', function () {
 Router.route("/admin/", function(){
   this.render("layoutAdminMain");
 });
+/*Admin Routes*/
+Router.route("/admin/", function(){
+  this.render("layoutAdminOverlay");
+});
+Router.route("/admin/", function(){
+  this.render("layoutAdminInteraction");
+});
+Router.route("/admin/", function(){
+  this.render("layoutAdminInformation");
+});
+Router.route("/admin/", function(){
+  this.render("layoutAdminCustomize");
+});
+/*Client Routes*/
+Router.route("/client/", function(){
+  this.render("layoutClientMain");
+});
+Router.route("/client/", function(){
+  this.render("layoutClientInformation");
+});
