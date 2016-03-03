c04d5d912897210615198a7cfaf7f8b73a580e7d
Jonas Faber <jonas.faber@iem.thm.de>
2016-03-03 17:06:45 +0100

    Add confirm and notifications for plugin editing

--- src/client/modules/admin/content/customize/customizeEditPlugins.js
+++ src/client/modules/admin/content/customize/customizeEditPlugins.js
@@ -1,9 +1,32 @@
 
 Template.customizeEditPlugins.helpers({
   plugins: function(){
-    return Plugins.find({});
+    return Plugins.find({},{
+      sort: {
+        type:-1
+      }
+    });
   },
   infos: function(plugin_id){
     return Info.find({plugin_id:plugin_id});
-  }
+  },
+    onError: function () {
+      return function (error) {
+        toastr.error("","Error on delete." );
+      };
+    },
+    onSuccess: function () {
+      return function (result) {
+        toastr.success("", "Item deleted.");
+      };
+    },
+    beforeRemove: function () {
+      return function (collection, id) {
+        var doc = collection.findOne(id);
+        var context = this;
+        var bModal = BModals.showNew('customizeConfirmModal', {
+          context:context
+        });
+      };
+    }
 });
