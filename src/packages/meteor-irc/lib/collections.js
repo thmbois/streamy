837752273aa5041a3121a15be405fdc91051b326
Jonas Faber <dev.jonas.faber@gmail.com>
2016-03-23 23:29:27 +0100

    Remove old irc plugin.

--- src/packages/meteor-irc/lib/collections.js
+++
@@ -1,4 +0,0 @@
-IRCMessages = new Mongo.Collection('ircMessages');
-IRCChannels = new Mongo.Collection('ircChannels');
-IRCConnections = new Mongo.Collection('ircConnections');
-IRCLinks = new Mongo.Collection('ircLinks');
