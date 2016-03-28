ff1d7b1fd3321b29bb0c4fa20d10912550c428bd
Jonas Faber <dev.jonas.faber@gmail.com>
2016-03-28 19:52:43 +0200

    Fix Error configs could be null.

--- src/server/modules/plugins/xplugins.js
+++ src/server/modules/plugins/xplugins.js
@@ -33,21 +33,25 @@ Tracker.autorun(function(){
   var twitterPlugins = Plugins.find({type:"twitter"}, {fields: {configs: 1, type:1}}).fetch();
   twitterWall.stopAllStreams();
   twitterPlugins.forEach(function(element) {
-    var config = element.configs.filter(function (el) {
-      return el.config === "twitterHashtag";
-    });
-    var hashtag = config[0].value;
-    twitterWall.start(hashtag);
+    if (element.configs) {
+      var config = element.configs.filter(function (el) {
+        return el.config === "twitterHashtag";
+      });
+      var hashtag = config[0].value;
+      twitterWall.start(hashtag);
+    }
   });
 });
 Tracker.autorun(function(){
   var chatPlugins = Plugins.find({type:"chat"}, {fields: {configs: 1, type:1}}).fetch();
   chatBot.stopAllBots();
   chatPlugins.forEach(function(element) {
-    var config = element.configs.filter(function (el) {
-      return el.config === "twitchChannel";
-    });
-    var channel = config[0].value;
-    chatBot.join("#"+channel);
+    if (element.configs) {
+      var config = element.configs.filter(function (el) {
+        return el.config === "twitchChannel";
+      });
+      var channel = config[0].value;
+      chatBot.join("#"+channel);
+    }
   });
 });
